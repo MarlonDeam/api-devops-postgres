55bdc0b97900e2f7bfdb4e83478486d26423bbab
MarlonDeam <marlonnobre.dev@gmail.com>
2026-08-12 18:44:12 -0300

    Tentativa de resolução de erro #1

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,6 +4,6 @@ from main import app
 client = TestClient(app)
 
 def test_healthcheck():
-	response = client.get("/")
-	assert response.status_code == 200
-	assert response.json() = = {"status": "ok"}
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"status": "ok"}
